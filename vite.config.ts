8a0656b56328dccef5b5810a854009c59cb219f5
Fucsko Norbert <fucskonorbi@gmail.com>
2025-01-01 23:14:16 +0100

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' 
-    ? process.env.VITE_BASE_URL || '/csengeesnorbi.hu/'  // Default to repo name for GitHub Pages
-    : '/',  // Use root path for development
+  base: "/csengeesnorbi.hu/",
 })
