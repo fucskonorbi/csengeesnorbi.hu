21e4b46f1c732178341c73c0277a556de98681e4
Fucsko Norbert <fucskonorbi@gmail.com>
2025-01-02 00:16:53 +0100

    Try another fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/csengeesnorbi.hu/',
+  base: '/fucskonorbi.github.io/csengeesnorbi.hu/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
